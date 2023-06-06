2990e3cb806a7e594fc26bfc752274bdcf192240
Drew Rowan <andrew.rowan@wallee.com>
2023-06-06 11:24:15 +0200

    Release 5.0.0

--- src/Resources/app/administration/src/main.js
+++
@@ -1,3 +0,0 @@
-import './module/wallee-order';
-import './module/wallee-settings';
-import './init/api-service.init';
--- src/Resources/app/administration/src/module/wallee-settings/component/sw-wallee-settings-icon/index.js
+++
@@ -1,7 +0,0 @@
-import template from './index.html.twig';
-
-const { Component } = Shopware;
-
-Component.register('sw-wallee-settings-icon', {
-    template
-});
